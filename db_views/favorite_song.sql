150859c1fb752c8a592aa20962e2d52bdbb9461d
KaushalSheth <shethk2006@gmail.com>
2021-11-13 10:13:42 -0800

    Updated favorite_song view to change calculation of adjusted score

--- db_views/favorite_song.sql
+++ db_views/favorite_song.sql
@@ -3,13 +3,24 @@ CREATE OR REPLACE VIEW favorite_song AS
 with
 perf_day_counts as (
 	select a.fixed_title,a.created_at,
+		-- Older counts are worth less than recent counts, so reduce older counts accordingly
+		-- 0-180: full value, 181-365: 3/4, 366-730: 1/2, 366+: 1/4
+		case
+			when date_part('day', now() - a.created_at) <= 180 then 1.0
+			when date_part('day', now() - a.created_at) <= 365 then 0.75
+			when date_part('day', now() - a.created_at) <= 730 then 0.5
+			else 0.25
+		end adjust_rate,
 		count(case when b.created_at <= a.created_at + interval '30 days' then 1 else null end) as perf_30day_cnt,
 		count(case when b.created_at <= a.created_at + interval '10 days' then 1 else null end) as perf_10day_cnt,
 		count(case when b.created_at <= a.created_at + interval '5 days' then 1 else null end) as perf_5day_cnt,
 		count(case when b.created_at <= a.created_at + interval '1 days' then 1 else null end) as perf_1day_cnt
 	from my_performances a
 		inner join my_performances b on b.fixed_title = a.fixed_title and b.created_at between a.created_at and a.created_at + interval '30 days'
-	group by 1,2
+	-- Exclude joins because that is not something I control and should not factor into favorite song
+	where 	a.join_ind = 0
+	and 	b.join_ind = 0
+	group by 1,2, 3
 	),
 perf_stats as (
 	select 	fixed_title, first_performance_time, current_month_ind,
@@ -25,11 +36,10 @@ perf_stats as (
 						max(perf_10day_cnt) as perf_10day_cnt,
 						max(perf_5day_cnt) as perf_5day_cnt,
 						max(perf_1day_cnt) as perf_1day_cnt,
-						-- Older counts are worth less than recent counts, so reduce older counts accordingly
-						max(greatest(0,(perf_30day_cnt::float - date_part('day',now()-created_at)/20.0))) as adj_perf_30day_cnt,
-						max(greatest(0,(perf_10day_cnt::float - date_part('day',now()-created_at)/30.0))) as adj_perf_10day_cnt,
-						max(greatest(0,(perf_5day_cnt::float - date_part('day',now()-created_at)/50.0))) as adj_perf_5day_cnt,
-						max(greatest(0,(perf_1day_cnt::float - date_part('day',now()-created_at)/100.0))) as adj_perf_1day_cnt,
+						max(perf_30day_cnt::float * adjust_rate) as adj_perf_30day_cnt,
+						max(perf_10day_cnt::float * adjust_rate) as adj_perf_10day_cnt,
+						max(perf_5day_cnt::float * adjust_rate) as adj_perf_5day_cnt,
+						max(perf_1day_cnt::float * adjust_rate) as adj_perf_1day_cnt,
 						count(*) as perf_cnt
 				from 	perf_day_counts
 				group by 1
@@ -38,18 +48,18 @@ perf_stats as (
 select 	ps.fixed_title, ps.first_performance_time, ps.perf_cnt,
 		ps.perf_30day_cnt, ps.perf_10day_cnt, ps.perf_5day_cnt, ps.perf_1day_cnt,
 		(
-			(ps.adj_perf_1day_cnt::decimal*300) +
-			(ps.adj_perf_5day_cnt::decimal*60) +
-            (ps.adj_perf_10day_cnt::decimal*30) +
-            (ps.adj_perf_30day_cnt::decimal*10) +
-			ps.perf_cnt
+			(ps.adj_perf_1day_cnt::decimal*20) +
+			(ps.adj_perf_5day_cnt::decimal*10) +
+            (ps.adj_perf_10day_cnt::decimal*6) +
+            (ps.adj_perf_30day_cnt::decimal*3) +
+			(ps.perf_cnt::decimal)
 		) adj_weighted_cnt,
 		(
-			(ps.perf_1day_cnt::decimal*300) +
-			(ps.perf_5day_cnt::decimal*60) +
-            (ps.perf_10day_cnt::decimal*30) +
-            (ps.perf_30day_cnt::decimal*10) +
-			ps.perf_cnt
+			(ps.perf_1day_cnt::decimal*20) +
+			(ps.perf_5day_cnt::decimal*10) +
+            (ps.perf_10day_cnt::decimal*6) +
+            (ps.perf_30day_cnt::decimal*3) +
+			(ps.perf_cnt::decimal)
 		) weighted_cnt,
 		ps.current_month_ind
 from 	perf_stats ps
