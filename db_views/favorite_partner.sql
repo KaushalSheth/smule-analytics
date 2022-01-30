6df29f65b9c66367a39c211c53cc1e84f37ad332
KaushalSheth <shethk2006@gmail.com>
2022-01-30 15:04:00 -0800

    Updated logic for rating for Favorite Partners analytics

--- db_views/favorite_partner.sql
+++ db_views/favorite_partner.sql
@@ -2,11 +2,12 @@ drop view favorite_partner;
 CREATE OR REPLACE VIEW favorite_partner AS
 with
 perf as (
-    select  p.*,
+    select  p.*, pf.rating_nbr,
             greatest(1,30-p.days_since_performance) as performance_weight_nbr,
             case when l.item_name is not null then 1 else 0 end always_include_ind
     from    my_performances p
     left outer join smule_list l on l.list_type = 'INCLUDE_PARTNER' and l.item_name ilike p.performers
+    left outer join performance_favorite pf on pf.performance_key = p.key
             -- Eclude my performances
     where   performers != 'KaushalSheth1'
             -- Exclude performers in the exclude list
@@ -25,7 +26,9 @@ perf_stats as (
             max(case when join_ind = 0 then created_at else '2000-01-01'::timestamp end) as last_performance_time,
             min(created_at) as first_performance_time,
             min(case when join_ind = 1 then created_at else null end) as first_join_time,
-            max(case when join_ind = 1 then created_at else null end) as last_join_time
+            max(case when join_ind = 1 then created_at else null end) as last_join_time,
+            count(case when rating_nbr < 5 then 1 else null end) as rated_song_cnt,
+            round(coalesce(avg(rating_nbr),0.0),2) avg_rating_nbr
     from 	perf
     group by 1, 2, 3
 )
@@ -49,13 +52,19 @@ select 	p.partner_account_id, p.partner_name, p.performance_cnt, p.join_cnt, p.f
         s.pic_url as display_pic_url,
         coalesce(sf.is_following,false) as is_following,
         extract(day from first_join_time - first_performance_time) days_till_first_join,
-        first_join_time, last_join_time
+        first_join_time, last_join_time,
+        round(case
+            when performance_cnt <= 5 then avg_rating_nbr - 1 -- Not enough performances to get accurate rating, so subtract 1
+            when rated_song_cnt = 0 then avg_rating_nbr - greatest((0.5 - favorite_cnt/(performance_cnt*1.0)),0)
+            else avg_rating_nbr
+        end, 2) as avg_rating_nbr,
+        rated_song_cnt
 from 	perf_stats p
         left outer join singer s on s.account_id = p.partner_account_id
         left outer join singer_following sf on sf.account_id = p.partner_account_id
 -- Include all users I'm following with whom I don't have any performances yet
 UNION ALL
-select  account_id as partner_account_id, handle as partner_name, 0, 0, 0, 0, 0, 0, 1, 99999, 99999, '1900-01-01'::timestamp, '1900-01-01'::timestamp, pic_url, is_following, null, null, null
+select  account_id as partner_account_id, handle as partner_name, 0, 0, 0, 0, 0, 0, 1, 99999, 99999, '1900-01-01'::timestamp, '1900-01-01'::timestamp, pic_url, is_following, null, null, null, 0.0, 0
 from    singer_following
 where   is_following and is_vip
 and     handle not in (select performers from my_performances)
